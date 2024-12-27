3ba2c81ff6e1db89bdfb167ae12182f10718a3f5
kavin <kavin.ned@gmail.com>
2024-12-27 21:49:18 +0800

    login and register api route

--- src/api/routes/userRoutes.ts
+++ src/api/routes/userRoutes.ts
@@ -1,5 +1,7 @@
-import express from "express";
+import express, { RequestHandler } from "express";
 import { User } from "../../models/User";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -12,17 +14,39 @@ router.get("/", async (_req, res) => {
     }
 });
 
-router.post("/", async (req, res) => {
-    try {
-        const newUser = new User({
-            username: req.body.username,
-        });
+router.post("/register", async (req, res) => {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-    } catch (error) {
-        res.status(500).json({ error: `${error} Failed to create user` });
-    }
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+
+    res.json({
+        message: "User has been created",
+        email,
+        hashedPassword,
+    }).status(201);
 });
+
+const loginHandler: RequestHandler = async (req, res) => {
+    const jwtSecret = process.env.JWT_SECRET as string;
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const passwordValid = await bcrypt.compare(password, user!.password);
+
+    if (!user || !passwordValid) {
+        res.status(401).json({ error: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ userId: user?._id }, jwtSecret, {
+        expiresIn: process.env.JWT_EXPIRATION,
+    });
+
+    res.cookie("token", token, {
+        httpOnly: true,
+        maxAge: 3600 * 1000,
+    }).json({ message: "Login successful" });
+};
+router.post("/login", loginHandler);
 
 export default router;
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,12 +2,14 @@ import { Schema, Document, model, Types } from "mongoose";
 
 interface User extends Document {
     email: string;
+    password: string;
     name: string;
     itineraries: Types.ObjectId[];
 }
 
 const userSchema = new Schema<User>({
     email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     name: { type: String, required: true },
     itineraries: [{ type: Types.ObjectId, ref: "Itinery" }],
 });
