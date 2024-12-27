d289fe6c412e1953318da489f2e140dd4a2cfa7e
kavin <kavin.ned@gmail.com>
2024-12-27 23:45:51 +0800

    testing api routes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,43 @@
 export default function Index() {
-    return <div>Helaur Waurld</div>;
+    const handleGet = () => {
+        fetch("http://localhost:3000/api/users", {
+            method: "GET",
+            credentials: "include",
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    };
+
+    const handleLogin = () => {
+        fetch("http://localhost:3000/api/users/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify({
+                email: "a@a.com",
+                password: "12345",
+                // name: "ironman",
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    };
+
+    return (
+        <div>
+            <button onClick={handleGet} className="bg-sky-500">
+                Get /
+            </button>
+            <br />
+            <br />
+            <button onClick={() => handleLogin()} className="bg-sky-500">
+                Login
+            </button>
+            <br />
+            <br />
+            <button className="bg-sky-500">Register</button>
+        </div>
+    );
 }
