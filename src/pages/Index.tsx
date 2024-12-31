4410654471c306213bcd8eb465850480d15b5db8
kavin <kavin.ned@gmail.com>
2024-12-31 18:42:34 +0800

    remove unecessary things

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,43 +1,7 @@
 export default function Index() {
-    const handleGet = () => {
-        fetch("http://localhost:3000/api/users", {
-            method: "GET",
-            credentials: "include",
-        })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    };
-
-    const handleLogin = () => {
-        fetch("http://localhost:3000/api/users/login", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-            body: JSON.stringify({
-                email: "a@a.com",
-                password: "12345",
-                // name: "ironman",
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    };
-
     return (
-        <div>
-            <button onClick={handleGet} className="bg-sky-500">
-                Get /
-            </button>
-            <br />
-            <br />
-            <button onClick={() => handleLogin()} className="bg-sky-500">
-                Login
-            </button>
-            <br />
-            <br />
-            <button className="bg-sky-500">Register</button>
+        <div className="flex items-center justify-center h-screen w-screen">
+            <h1 className="text-4xl">Welcome to Roamify</h1>
         </div>
     );
 }
