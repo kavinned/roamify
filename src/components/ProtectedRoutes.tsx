c83c74d20ded8552e98828d8ad6183a0d5447b45
kavin <kavin.ned@gmail.com>
2024-12-31 22:37:07 +0800

    remove unneeded effect

--- src/components/ProtectedRoutes.tsx
+++ src/components/ProtectedRoutes.tsx
@@ -1,23 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { useAppSelector } from "../store/store";
-import { useEffect } from "react"; // Import useEffect
 
 export default function ProtectedRoutes() {
     const isAuth = useAppSelector((state) => state.auth.isAuth);
 
-    useEffect(() => {
-        if (!isAuth) {
-            // Set a timeout to redirect after 2 seconds
-            const timer = setTimeout(() => {
-                window.location.href = "/login";
-            }, 2000);
-
-            // Cleanup the timer if the component unmounts
-            return () => clearTimeout(timer);
-        }
-    }, [isAuth]); // Run this effect when `isAuth` changes
-
     if (!isAuth) {
+        setTimeout(() => {
+            window.location.href = "/login";
+        }, 2000);
+
         return (
             <div className="flex flex-col gap-5 w-screen h-screen z-50 items-center justify-center text-3xl">
                 <p>Unauthorized. Redirecting to login...</p>
