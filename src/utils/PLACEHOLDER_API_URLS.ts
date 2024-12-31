8dc7cfcdba9e54ede9dc5bb1f21df0209190fd98
kavin <kavin.ned@gmail.com>
2025-01-01 01:30:06 +0800

    add placeholder fake api urls

---
+++ src/utils/PLACEHOLDER_API_URLS.ts
@@ -0,0 +1,17 @@
+function iqueryDestinationURL() {
+    return `http://localhost:3001/destinations`;
+}
+
+function igetDestinationHotelsURL() {
+    return `http://localhost:3001/hotels`;
+}
+
+function igetTouristAttractionsURL() {
+    return `http://localhost:3001/places`;
+}
+
+export {
+    iqueryDestinationURL,
+    igetDestinationHotelsURL,
+    igetTouristAttractionsURL,
+};
