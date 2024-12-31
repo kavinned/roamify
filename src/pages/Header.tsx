1c7c6dba806a8b8cade1f8a1c26a2b05199efd2d
kavin <kavin.ned@gmail.com>
2024-12-31 18:53:08 +0800

    add login register buttons and nav styling

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -4,8 +4,24 @@ export default function Header() {
     return (
         <header>
             <nav>
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/dashboard">Dashboard</NavLink>
+                <span className="nav-section">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/dashboard">Dashboard</NavLink>
+                </span>
+                <span className="nav-section auth-btns">
+                    <NavLink
+                        className="bg-green-600 px-2 py-1 rounded-xl font-bold text-sm text-black text-opacity-85 hover:bg-green-700 border-2 border-opacity-50 border-slate-950"
+                        to="/login"
+                    >
+                        Login
+                    </NavLink>
+                    <NavLink
+                        className="bg-sky-600 px-2 py-1 rounded-xl font-bold text-sm text-black text-opacity-85 hover:bg-sky-700 border-2 border-opacity-50 border-slate-950"
+                        to="/register"
+                    >
+                        Register
+                    </NavLink>
+                </span>
             </nav>
         </header>
     );
