a37d2355aca7beb12553c750387963e985e686c7
kavin <kavin.ned@gmail.com>
2025-01-03 17:10:39 +0800

    add logout handler and conditionally render buttons

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -1,6 +1,18 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { logoutThunk } from "../store/thunks/authThunks";
 
 export default function Header() {
+    const { isAuth } = useAppSelector((state) => state.auth);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    async function handleLogout(event: { preventDefault: () => void }) {
+        event.preventDefault();
+        await dispatch(logoutThunk());
+        navigate("/login");
+    }
+
     return (
         <header>
             <nav>
@@ -9,18 +21,30 @@ export default function Header() {
                     <NavLink to="/dashboard">Dashboard</NavLink>
                 </span>
                 <span className="nav-section auth-btns">
-                    <NavLink
-                        className="bg-green-600 px-2 py-1 rounded-xl font-bold text-sm text-black text-opacity-85 hover:bg-green-700 border-2 border-opacity-50 border-slate-950"
-                        to="/login"
-                    >
-                        Login
-                    </NavLink>
-                    <NavLink
-                        className="bg-sky-600 px-2 py-1 rounded-xl font-bold text-sm text-black text-opacity-85 hover:bg-sky-700 border-2 border-opacity-50 border-slate-950"
-                        to="/register"
-                    >
-                        Register
-                    </NavLink>
+                    {!isAuth ? (
+                        <>
+                            <NavLink
+                                className="bg-green-600 px-2 py-1 rounded-xl font-bold text-sm text-black text-opacity-85 hover:bg-green-700 border-2 border-opacity-50 border-slate-950"
+                                to="/login"
+                            >
+                                Login
+                            </NavLink>
+                            <NavLink
+                                className="bg-sky-600 px-2 py-1 rounded-xl font-bold text-sm text-black text-opacity-85 hover:bg-sky-700 border-2 border-opacity-50 border-slate-950"
+                                to="/register"
+                            >
+                                Register
+                            </NavLink>
+                        </>
+                    ) : (
+                        <button
+                            className="bg-green-600 px-2 py-1 rounded-xl font-bold text-sm text-black text-opacity-85 hover:bg-green-700 border-2 border-opacity-50 border-slate-950"
+                            type="button"
+                            onClick={handleLogout}
+                        >
+                            Logout
+                        </button>
+                    )}
                 </span>
             </nav>
         </header>
