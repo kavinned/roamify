f9de1648f7847e381b897ba1bfa6cc945691c604
kavin <kavin.ned@gmail.com>
2025-01-07 23:24:03 +0800

    add click handler to search results for city navigation and data fetching

--- src/components/SearchResultsList.tsx
+++ src/components/SearchResultsList.tsx
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import { Results } from "../store/reducers/searchSlice";
-import { useAppSelector } from "../store/store";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { cityThunk } from "../store/thunks/cityThunk";
 import Loader from "./Loader";
 
 export default function SearchResultsList() {
     const { results, status } = useAppSelector((state) => state.search);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    function handleClick(event: React.MouseEvent<HTMLLIElement>) {
+        event.preventDefault();
+        const cityName = event.currentTarget.textContent;
+        if (cityName) dispatch(cityThunk(cityName));
+        navigate(`/city?name=${cityName}`);
+        return;
+    }
 
     return (
         <>
@@ -12,6 +24,7 @@ export default function SearchResultsList() {
                 <ul className="p-5 md:w-1/3 w-2/3 text-center border-2 border-black ">
                     {results.map((result: Results) => (
                         <li
+                            onClick={handleClick}
                             className="p-2 border border-black m-2 rounded-lg cursor-pointer hover:bg-slate-300"
                             key={`${result.name}${result.lat}`}
                         >
