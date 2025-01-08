18646606f7dcf22fa4506250f9ef5aefc3409f9a
kavin <kavin.ned@gmail.com>
2025-01-09 00:11:13 +0800

    add endpoint to fetch nearby places based on latitude and longitude

--- src/api/routes/cityRoutes.ts
+++ src/api/routes/cityRoutes.ts
@@ -47,4 +47,34 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get("/places", async (req, res) => {
+    const { lat, lng } = req.query;
+
+    if (!lat || !lng) {
+        res.status(400).json({
+            message: "Please provide latitude and longitude",
+        });
+        return;
+    }
+
+    // const placesURL = `https://trueway-places.p.rapidapi.com/FindPlacesNearby?location=${lat}%2C${lng}&type=tourist_attraction&radius=10000&language=en`;
+    const placesURL = `http://localhost:3001/places`;
+    const options = {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Key": process.env.RAPIDAPI_KEY as string,
+            "X-RapidAPI-Host": process.env.TRUEWAY_HOST as string,
+        },
+    };
+
+    try {
+        const response = await fetch(placesURL, options);
+        const data = await response.json();
+        res.json(data);
+        return;
+    } catch (error) {
+        res.status(500).json({ error: `${error} Failed to fetch` });
+    }
+});
+
 export default router;
