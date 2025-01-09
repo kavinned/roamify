95e4966dcce6e89a7aa29123f2cf60931d9b7bc3
kavin <kavin.ned@gmail.com>
2025-01-10 01:41:58 +0800

    refactor HotelSearch component to integrate loading state and dispatch hotelThunk for date selection

--- src/components/HotelSearch.tsx
+++ src/components/HotelSearch.tsx
@@ -1,31 +1,50 @@
-import { hotelActions } from "../store/reducers/hotelSlice";
-import { useAppDispatch } from "../store/store";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { hotelThunk } from "../store/thunks/cityThunk";
+import SmallLoader from "./SmallLoader";
 
 export default function HotelSearch() {
+    const { entityId } = useAppSelector((state) => state.city);
+    const { status } = useAppSelector((state) => state.hotel);
     const dispatch = useAppDispatch();
 
     async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
         const formObject = Object.fromEntries(formData);
-        const checkInDate = formObject.checkinDate;
-        const checkOutDate = formObject.checkoutDate;
-        dispatch(hotelActions.setDate({ checkInDate, checkOutDate }));
+        const checkInDate = formObject.checkinDate as string;
+        const checkOutDate = formObject.checkoutDate as string;
+        if (entityId)
+            dispatch(hotelThunk({ entityId, checkInDate, checkOutDate }));
     }
-
     return (
         <form
             onSubmit={handleSubmit}
             className="hotel-search-form flex flex-1 justify-center items-center"
         >
             <div className="flex flex-col">
-                <label htmlFor="checkin-date">Check In</label>
-                <input type="date" name="checkinDate" id="checkin-date" />
-                <label htmlFor="checkout-date">Check Out</label>
-                <input type="date" name="checkoutDate" id="checkout-date" />
-                <button className="self-center" type="submit">
-                    Search
-                </button>
+                {status === "loading" ? (
+                    <SmallLoader />
+                ) : (
+                    <>
+                        <label htmlFor="checkin-date">Check In</label>
+                        <input
+                            type="date"
+                            name="checkinDate"
+                            id="checkin-date"
+                            required
+                        />
+                        <label htmlFor="checkout-date">Check Out</label>
+                        <input
+                            type="date"
+                            name="checkoutDate"
+                            id="checkout-date"
+                            required
+                        />
+                        <button className="self-center" type="submit">
+                            Search
+                        </button>
+                    </>
+                )}
             </div>
         </form>
     );
