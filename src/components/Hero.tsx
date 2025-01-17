b80817f7bd68fbe0d1d65ddca71931b66dd44423
kavin <kavin.ned@gmail.com>
2025-01-18 02:23:43 +0800

    feat: enhance Hero component with animated text effects and loading state; add text-shadow utilities

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,40 +1,89 @@
-import SmallLoader from "./SmallLoader";
+import { useState } from "react";
+import { TextAnimate } from "./ui/text-animate";
 
 export default function Hero() {
+    const [imageLoaded, setImageLoaded] = useState(false);
+
     return (
-        <div className="relative overflow-hidden w-full bg-zinc-900 py-24 sm:py-32">
-            <SmallLoader classes="absolute w-full h-full top-1/2 left-1/2 -z-10" />
+        <div className="relative overflow-hidden w-full bg-zinc-900 py-24 sm:py-32 min-h-72 max-h-72 mt-12 flex items-center justify-start">
+            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div className="h-12 w-12 rounded-full border-8 border-b-transparent border-teal-500 animate-spin"></div>
+            </div>
             <img
                 src="assets/hero-image.jpg"
                 alt=""
                 className="absolute inset-0 w-screen object-cover object-top opacity-100 mix-blend-plus-darker h-full z-1"
+                loading="eager"
+                onLoad={() => {
+                    setImageLoaded(true);
+                }}
             />
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto max-w-2xl lg:mx-0">
-                    <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl mix-blend-difference">
-                        Explore the World
-                    </h2>
-                    <p className="mt-6 text-xl leading-8 text-gray-300 mix-blend-difference font-medium">
-                        Discover amazing places and plan your perfect trip.
+            {imageLoaded && (
+                <div>
+                    <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                        <div className="mx-auto max-w-2xl lg:mx-0">
+                            <TextAnimate
+                                className="font-black text-8xl text-white mix-blend-difference tracking-wide leading-snug text-shadow-gradient transition-[text-shadow] duration-500 ease-in-out"
+                                as="h1"
+                                animation="fadeIn"
+                            >
+                                Roamify
+                            </TextAnimate>
+                            <TextAnimate
+                                className="text-3xl font-bold tracking-tight text-white sm:text-6xl mix-blend-difference"
+                                animation="slideLeft"
+                                delay={0.1}
+                                as="h2"
+                                by="character"
+                                startOnView={true}
+                            >
+                                Explore the World
+                            </TextAnimate>
+                            <TextAnimate
+                                className="mt-6 text-xl leading-8 text-gray-300 mix-blend-difference font-medium"
+                                animation="slideDown"
+                                delay={0.3}
+                                as="p"
+                                by="word"
+                                startOnView={true}
+                            >
+                                Discover amazing places and plan your perfect
+                                trip.
+                            </TextAnimate>
+                        </div>
+                    </div>
+                    <p className="text-white font-thin text-[0.7rem] opacity-50 hover:opacity-100 transition-opacity duration-300 absolute bottom-5 right-5 leading-3">
+                        <TextAnimate delay={0.5} animation="blurIn" as="span">
+                            Photo by
+                        </TextAnimate>
+                        <a
+                            className="imgcredit hover:underline duration-300 transition-all"
+                            href="https://unsplash.com/@cikstefan?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+                        >
+                            <TextAnimate
+                                delay={0.5}
+                                animation="blurIn"
+                                as="span"
+                            >
+                                Štefan Štefančík
+                            </TextAnimate>
+                        </a>{" "}
+                        on{" "}
+                        <a
+                            className="imgcredit hover:underline duration-300 transition-all"
+                            href="https://unsplash.com/photos/man-standing-on-cliff-near-falls-0wMmxNB6Xzc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
+                        >
+                            <TextAnimate
+                                delay={0.5}
+                                animation="blurIn"
+                                as="span"
+                            >
+                                Unsplash
+                            </TextAnimate>
+                        </a>
                     </p>
                 </div>
-            </div>
-            <p className="text-white font-thin text-[0.7rem] opacity-50 hover:opacity-100 transition-opacity duration-300 absolute bottom-5 right-5 leading-3">
-                Photo by{" "}
-                <a
-                    className="imgcredit hover:underline duration-300 transition-all"
-                    href="https://unsplash.com/@cikstefan?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
-                >
-                    Štefan Štefančík
-                </a>{" "}
-                on{" "}
-                <a
-                    className="imgcredit hover:underline duration-300 transition-all"
-                    href="https://unsplash.com/photos/man-standing-on-cliff-near-falls-0wMmxNB6Xzc?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash"
-                >
-                    Unsplash
-                </a>
-            </p>
+            )}
         </div>
     );
 }
