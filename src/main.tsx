d9708dbe43e00efe0cf2210c103509ef43c5dad7
kavin <kavin.ned@gmail.com>
2025-01-17 19:30:05 +0800

    add ThemeProvider and ModeToggle components for theme management

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import store from "./store/store.ts";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "./components/ThemeProvider.tsx";
+import ModeToggle from "./components/ModeToggle";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Provider store={store}>
-            <App />
+            <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+                <App />
+                <ModeToggle />
+            </ThemeProvider>
         </Provider>
     </StrictMode>
 );
