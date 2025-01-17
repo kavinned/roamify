a992656121e6ce58fd99210fbadcd36d73faed94
kavin <kavin.ned@gmail.com>
2025-01-18 01:26:08 +0800

    refactor: remove Loader component and unused auth status from App

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,9 @@ import Header from "./components/Header";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import ProtectedRoutes from "./components/ProtectedRoutes";
-import { useAppDispatch, useAppSelector } from "./store/store";
+import { useAppDispatch } from "./store/store";
 import { useEffect } from "react";
 import { checkAuthStatus } from "./store/thunks/authThunk";
-import Loader from "./components/Loader";
 import Search from "./pages/Search";
 import City from "./pages/City";
 import DraftItinerary from "./pages/DraftItinerary";
@@ -16,15 +15,10 @@ import Itineraries from "./pages/Itineraries";
 
 function App() {
     const dispatch = useAppDispatch();
-    const { status } = useAppSelector((state) => state.auth);
 
     useEffect(() => {
         dispatch(checkAuthStatus());
     }, [dispatch]);
-
-    if (status === "loading") {
-        return <Loader />;
-    }
 
     return (
         <Router>
