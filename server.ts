d7ccc0405ebbc39bc7a38bbf120101e08c1f817c
kavin <kavin.ned@gmail.com>
2025-01-19 00:04:55 +0800

    revert server edit vercel

--- server.ts
+++ server.ts
@@ -6,7 +6,6 @@ import userRoutes from "./src/api/routes/userRoutes";
 import itineraryRoutes from "./src/api/routes/itineraryRoutes";
 import searchRoutes from "./src/api/routes/searchRoutes";
 import cityRoutes from "./src/api/routes/cityRoutes";
-import path from "path";
 
 dotenv.config();
 
@@ -33,8 +32,10 @@ app.use("/api/itineraries", itineraryRoutes);
 app.use("/api/search", searchRoutes);
 app.use("/api/city", cityRoutes);
 
-app.get("*", (_req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+app.use(express.static("dist"));
+
+app.get("*", (req, res) => {
+    res.sendFile("index.html", { root: "dist" });
 });
 
 app.listen(port, () => {
