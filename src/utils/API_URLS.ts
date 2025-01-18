138d9b9cc910ec72f11a240716060426372cd21e
kavin <kavin.ned@gmail.com>
2025-01-18 21:43:34 +0800

    debug

--- src/utils/API_URLS.ts
+++ src/utils/API_URLS.ts
@@ -1,5 +1,8 @@
 export const BASE_URL = import.meta.env.VITE_API_URL;
 
+console.log(BASE_URL);
+
+
 function searchURL(cityName: string) {
     return `${BASE_URL}/api/search?query=${cityName}`;
 }
