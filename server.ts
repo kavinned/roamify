8c92f5533abb3ac46b9f8c70b98e006dd6ce65b2
kavin <kavin.ned@gmail.com>
2025-01-18 19:26:54 +0800

    add origin for production in cors

--- server.ts
+++ server.ts
@@ -14,7 +14,7 @@ const port = process.env.PORT || 3000;
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://roamify.pages.dev"],
         credentials: true,
     })
 );
