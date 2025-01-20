6330f99c7aacefae56210fc00674844a1ab49c92
kavin <kavin.ned@gmail.com>
2025-01-20 14:53:13 +0800

    add gradient to bentocard

--- src/components/ui/bento-grid.tsx
+++ src/components/ui/bento-grid.tsx
@@ -18,6 +18,10 @@ type BentoCardProps = {
     description: string;
     href: string;
     cta: string;
+    gradientColors?: string[];
+    enableGradient?: boolean;
+    animateGradient?: boolean;
+    gradientClassName?: string;
 };
 
 const BentoGrid = ({ children, className }: BentoGridProps) => {
@@ -41,46 +45,71 @@ const BentoCard = ({
     description,
     href,
     cta,
-}: BentoCardProps) => (
-    <div
-        key={name}
-        className={cn(
-            "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
-            // light styles
-            "bg-white shadow-xl drop-shadow-sm border border-gray-300/50",
-            // dark styles
-            "transform-gpu dark:bg-primary-foreground dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
-            className
-        )}
-    >
-        <div>{background}</div>
-        <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
-            <Icon className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" />
-            <h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
-                {name}
-            </h3>
-            <p className="max-w-lg text-neutral-400">{description}</p>
-        </div>
+    enableGradient,
+    gradientColors,
+    gradientClassName,
+    animateGradient,
+}: BentoCardProps) => {
+    console.log(gradientClassName);
 
+    return (
         <div
+            key={name}
             className={cn(
-                "pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100"
+                "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
+                // light styles
+                "bg-white shadow-xl drop-shadow-sm border border-gray-300/50",
+                // dark styles
+                "transform-gpu dark:bg-primary-foreground dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+                className
             )}
         >
-            <Button
-                variant="ghost"
-                asChild
-                size="sm"
-                className="pointer-events-auto"
+            <div>
+                {background}
+                {enableGradient && (
+                    <div
+                        className={cn(
+                            `absolute ${gradientClassName} rounded-full bg-radial-gradient blur-2xl`,
+                            animateGradient ? "animate-gradient" : ""
+                        )}
+                        style={{
+                            background: `radial-gradient(circle at top left, ${
+                                gradientColors
+                                    ? gradientColors.join(", ")
+                                    : "rgba(255, 255, 255, 0.2), transparent"
+                            })`,
+                        }}
+                    />
+                )}
+            </div>
+            <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
+                <Icon className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" />
+                <h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">
+                    {name}
+                </h3>
+                <p className="max-w-lg text-neutral-400">{description}</p>
+            </div>
+
+            <div
+                className={cn(
+                    "pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100"
+                )}
             >
-                <Link to={href}>
-                    {cta}
-                    <ArrowRightIcon className="ml-2 h-4 w-4" />
-                </Link>
-            </Button>
+                <Button
+                    variant="ghost"
+                    asChild
+                    size="sm"
+                    className="pointer-events-auto"
+                >
+                    <Link to={href}>
+                        {cta}
+                        <ArrowRightIcon className="ml-2 h-4 w-4" />
+                    </Link>
+                </Button>
+            </div>
+            <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10" />
         </div>
-        <div className="pointer-events-none absolute inset-0 transform-gpu transition-all duration-300 group-hover:bg-black/[.03] group-hover:dark:bg-neutral-800/10" />
-    </div>
-);
+    );
+};
 
 export { BentoCard, BentoGrid };
