aa4b56596efb5a3ce0e6c9f46e810a42fbcc6673
kavin <kavin.ned@gmail.com>
2025-01-20 14:51:47 +0800

    remove custom border-radius from config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,6 @@ export default {
             fontFamily: {
                 montserrat: ["Montserrat", "sans-serif"],
             },
-            borderRadius: {
-                lg: "var(--radius)",
-                md: "calc(var(--radius) - 2px)",
-                sm: "calc(var(--radius) - 4px)",
-            },
             colors: {
                 background: "hsl(var(--background))",
                 foreground: "hsl(var(--foreground))",
