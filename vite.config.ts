99152578b0384e8684f39f3de6ec43c5918c256a
kavin <kavin.ned@gmail.com>
2025-01-21 20:17:16 +0800

    add build optimizations to reduce chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,34 @@ export default defineConfig({
             "@": path.resolve(__dirname, "./src"),
         },
     },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    "vendor-react": [
+                        "react",
+                        "react-dom",
+                        "react-router-dom",
+                        "react-redux",
+                        "react-icons",
+                    ],
+                    "vendor-ui": [
+                        "@radix-ui/react-dialog",
+                        "@radix-ui/react-dropdown-menu",
+                        "@radix-ui/react-label",
+                        "@radix-ui/react-slot",
+                        "lucide-react",
+                    ],
+                    "vendor-utils": [
+                        "@reduxjs/toolkit",
+                        "class-variance-authority",
+                        "clsx",
+                        "tailwind-merge",
+                    ],
+                },
+            },
+        },
+        chunkSizeWarningLimit: 500, // in kbs
+        minify: true,
+    },
 });
